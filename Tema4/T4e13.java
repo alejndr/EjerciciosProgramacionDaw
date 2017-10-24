3e4ee0610d6fadd5c6897243216405d5e0793044
Alejndr <alejandro.iamv@gmail.com>
2017-10-24 09:20:08 +0200

    corregido ejercicio 13

--- Tema4/T4e13.java
+++ Tema4/T4e13.java
@@ -19,17 +19,17 @@ public class T4e13 {
     
     
     
-    if (a > b && b < c) {
+    if (a > b && b > c) {
     System.out.println("Tus numero ordenados: " + a + ", " + b + ", " + c);} 
-    else if (a > c && c < b) {
+    else if (a > c && c > b) {
     System.out.println("Tus numero ordenados: " + a + ", " + c + ", " + b);} 
-    else if (b > a && a < c) {
+    else if (b > a && a > c) {
     System.out.println("Tus numero ordenados: " + b + ", " + a + ", " + c);}
-    else if (b > c && c < a) {
+    else if (b > c && c > a) {
     System.out.println("Tus numero ordenados: " + b + ", " + c + ", " + a);}
-    else if (c > a && a < b) {
+    else if (c > a && a > b) {
     System.out.println("Tus numero ordenados: " + c + ", " + a + ", " + b);}
-    else if (c > b && b < a) {
+    else if (c > b && b > a) {
     System.out.println("Tus numero ordenados: " + c + ", " + b + ", " + a);}
   }
 }
