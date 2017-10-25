4bc3d19c89fcc17559d5a32611b3fda0d843d000
alejndr <alejandro.iamv@gmail.com>
2017-10-25 20:28:30 +0200

    Subido 23

--- Tema4/T4e23.java
+++ Tema4/T4e23.java
@@ -18,24 +18,46 @@ public class T4e23 {
   System.out.print("Introduce el tipo de IVA (general, reducido o superreducido): ");
   String iva = (System.console().readLine().toLowerCase());
   System.out.print("Introduzca el código promocional (nopro, mitad, meno5 o 5porc): ");
-  int promo = Integer.parseInt(System.console().readLine());
+  String promo = (System.console().readLine().toLowerCase());
+  
+  double promoAplicado = 0;
+  double ivaAplicado = 0;
   
   switch (iva) {
     case "general":
-      double ivaAplicado = ((base * 0.21) + base);
+      ivaAplicado = ((base * 0.21) + base);
       break;
     case "reducido":
-      double ivaAplicado = ((base * 0.10) + base);
+      ivaAplicado = ((base * 0.10) + base);
       break;
     case "superreducido":
-      double ivaAplicado = ((base * 0.04) + base);
+      ivaAplicado = ((base * 0.04) + base);
       break;
-
-    default:
-        
+    default: 
     }
   
   
+  
+  switch (promo) {
+    case "nopro":
+      promoAplicado = ivaAplicado;
+      break;
+    case "mitad":
+	  promoAplicado = ivaAplicado / 2;
+      break;
+    case "meno5":
+      promoAplicado = ivaAplicado - 5;
+      break;
+	 case "5porc":
+      promoAplicado = (ivaAplicado * 0.05) + ivaAplicado;
+      break;
+    default:
+    }
+  
+  System.out.printf("Base imponible %8.2f \n", base);
+  System.out.printf("Precio con IVA %s %8.2f \n", iva, ivaAplicado);
+  System.out.printf("Cód. promo. (%s) \n", promo);
+  System.out.printf("TOTAL %8.2f \n", promoAplicado);
   }
 }
 
