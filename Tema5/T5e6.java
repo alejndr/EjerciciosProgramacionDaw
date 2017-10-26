4c5966b2b48644c7d1c34fa08632f45f34509a62
Alejndr <alejandro.iamv@gmail.com>
2017-10-26 13:45:43 +0200

    añadido ejercicio 4, 5 y 6

---
+++ Tema5/T5e6.java
@@ -0,0 +1,18 @@
+/**
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+ * utilizando un bucle do while .
+ *
+ * @author Alejandro MV
+ */
+public class T5e6 {
+  public static void main(String[] args) {
+    
+    int i = 340;
+
+    do {
+    i = i-20;
+    System.out.println(i);
+      
+    } while (i >= 180);
+  }
+}
