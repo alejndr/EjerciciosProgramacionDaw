625dde3c018ad5ad7c200d2bbd730fc367440485
Alejndr <alejandro.iamv@gmail.com>
2017-10-26 13:05:28 +0200

    añadida carpeta de tema 5 y ejercicios 1 y 2

---
+++ Tema5/T5e2.java
@@ -0,0 +1,16 @@
+/**
+ * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while .
+ *
+ * @author Alejandro MV
+ */
+public class T5e2 {
+  public static void main(String[] args) {
+    
+    int i = 0;
+    
+    while (i <= 100) {
+      System.out.println(i);
+      i = i+5;
+    }
+  }
+}
