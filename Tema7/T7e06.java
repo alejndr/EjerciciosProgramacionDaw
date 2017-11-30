2495d2c24f3057330b7f2d5c5fbe4f482bb06722
Alejndr <alejandro.iamv@gmail.com>
2017-11-30 13:30:09 +0100

    6 a medias

---
+++ Tema7/T7e06.java
@@ -0,0 +1,39 @@
+/*
+ * Escribe un programa que lea 15 números por teclado y que los almacene en un
+ * array. Rota los elementos de ese array, es decir, el elemento de la posición 0
+ * debe pasar a la posición 1, el de la 1 a la 2, etc. El número que se encuentra en
+ * la última posición debe pasar a la posición 0. Finalmente, muestra el contenido
+ * del array.
+ * 
+ * @author Alejandro MV
+ */
+
+
+
+public class T7e06 {
+  public static void main(String[] args) {
+    
+  System.out.println("Introduce 15 numeros enteros por teclado: ");
+  
+  int aux = 0;
+  int[] array = new int[15];
+
+  for (int i = 0;i < 15;i++) {
+    array[i] = Integer.parseInt( System.console().readLine() );
+    
+  }
+
+  for (int i = 0;i < 15;i++) {
+    
+    array[i] = array[i+1];
+    
+  }
+
+  for (int i = 0;i < 15;i++) {
+  System.out.println(array[i]);
+  }
+  
+
+  }
+  
+}
