30021db9cfec5f4682ae1646ecb942ef096a3bd7
Alejndr <alejandro.iamv@gmail.com>
2017-11-30 12:41:08 +0100

    Ejercicios asta el 5 de tema 7

--- Tema7/T7e05.java
+++ Tema7/T7e05.java
@@ -9,11 +9,13 @@
 
 
 public class T7e05 {
-  public static void main(String[] args) {System.out.println("Introduce 10 numeros enteros por teclado: ");
+  public static void main(String[] args) {
+    
+  System.out.println("Introduce 10 numeros enteros por teclado: ");
   int maximo = 0;
   int minimo = Integer.MAX_VALUE;
   int mostrarArray = 0;
-  int[] array = new int[11];
+  int[] array = new int[10];
 
 
   for (int i = 0;i < 10;i++) {
@@ -29,7 +31,7 @@ public class T7e05 {
   System.out.println("Numeros introducidos con minimo y maximo señalados");
   for (int i = 0;i < 10;i++) {
     System.out.print(array[mostrarArray]);
-    mostrarArray++;
+    
 
     if (array[mostrarArray] == minimo) {
       System.out.print(" (minimo)");
@@ -38,7 +40,7 @@ public class T7e05 {
     if (array[mostrarArray] == maximo) {
       System.out.print(" (maximo)");
     }
-
+    mostrarArray++;
     System.out.println("");
 
   }
