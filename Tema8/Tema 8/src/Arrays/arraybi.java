cf43275a17e70def501692ea404982ab81e48044
Alejndr <alejandro.iamv@gmail.com>
2018-01-30 09:00:55 +0100

    funciones

--- Tema8/Tema 8/src/Arrays/arraybi.java
+++ Tema8/Tema 8/src/Arrays/arraybi.java
@@ -5,5 +5,17 @@ package Arrays;
  * @author alejandro
  */
 public class arraybi {
+
+  public static int[][] generaArrayBiInt(int f, int c, int min, int max){
+    int[][] resultado = new int [f][c];
+    
+    for (int fila = 0; fila < f; fila++) {
+      for (int columna = 0; columna < f; columna++) {
+        resultado[fila][columna] = (int)(Math.random() * (max - min) +1);
+      }
+    }
+      
+  return resultado;
   
+  }
 }
